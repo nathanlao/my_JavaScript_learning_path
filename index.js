7de1f84a4b379644c875bdc41d281a6b95044bdc
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-19 02:24:29 -0800

    Adding variable and math operation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.getElementById("count-el").innerText = 10 // Use js to get element by an id
+// document.getElementById("count-el").innerText = 10 // Use js to get element by its id
+
+// create a variable
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
+
+console.log(count)
+
+// math operation
+let age = 25
+let humanDogRatio = 7 
+let myDogAge = age * humanDogRatio
+console.log(myDogAge)
