3fdae48889dc2b7f413fd9a14a23de3851b09006
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-20 12:45:06 -0800

    ressign variable and increment variable

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // document.getElementById("count-el").innerText = 10 // Use js to get element by its id
 
-// create a variable
+// 1. create a variable
 let firstBatch = 5
 let secondBatch = 7
 let count = firstBatch + secondBatch
 
 console.log(count)
 
-// math operation
+// 2. math operation
 let age = 25
 let humanDogRatio = 7 
 let myDogAge = age * humanDogRatio
 console.log(myDogAge)
+
+// 3. Reassign variable
+count = 10
+console.log(count)
+
+// 4. Increment var
+count = count + 1
+console.log(count)
+
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
