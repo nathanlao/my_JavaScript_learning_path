0542a00cbff0a61282be21bbfccc12b2f46230ef
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-20 13:23:56 -0800

    Create a functon

---
+++ lesson9-/index.js
@@ -0,0 +1,18 @@
+// Setting up the count down
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown()
+
+// Create my function 
+function myPrintFunction() {
+    console.log(42)
+}
+
+myPrintFunction()
