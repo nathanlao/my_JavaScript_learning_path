a780e6143683c4b98ff6c44c4fbfeae4b7e729da
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-20 13:38:57 -0800

    Added function that increment the count

--- lesson1-8/index.js
+++ lesson1-8/index.js
@@ -1,37 +1,44 @@
 // document.getElementById("count-el").innerText = 10 // Use js to get element by its id
 
-// 1. create a variable
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch + secondBatch
+// // 1. create a variable
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
 
-console.log(count)
+// console.log(count)
 
-// 2. math operation
-let age = 25
-let humanDogRatio = 7 
-let myDogAge = age * humanDogRatio
-console.log(myDogAge)
+// // 2. math operation
+// let age = 25
+// let humanDogRatio = 7 
+// let myDogAge = age * humanDogRatio
+// console.log(myDogAge)
 
-// 3. Reassign variable
-count = 10
-console.log(count)
+// // 3. Reassign variable
+// count = 10
+// console.log(count)
 
-// 4. Increment var
-count = count + 1
-console.log(count)
+// // 4. Increment var
+// count = count + 1
+// console.log(count)
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
 
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
 
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
 
+// // Create a functnon for click event listener
+// function increment() {
+//     console.log("The button is clicked")
+// }
+
+let count = 0
 // Create a functnon for click event listener
-function increment() {
-    console.log("The button is clicked")
+function incrementCount() {
+    count = count + 1
+    console.log(count)
 }
