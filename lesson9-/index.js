424803f3ef78a952dc95d2232131c2ef513d0e07
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-20 13:33:06 -0800

    Function that increment

--- lesson9-/index.js
+++ lesson9-/index.js
@@ -27,3 +27,14 @@ function logSum() {
 }
 
 logSum()
+
+// Increment function
+let lapsCompleted = 0
+function incrementLap() {
+    lapsCompleted = lapsCompleted + 1
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
+console.log(lapsCompleted)
