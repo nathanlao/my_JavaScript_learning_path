e9c64bfc1905c9a23f924e13e1b01d5626d20c3d
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-20 16:55:04 -0800

    Added emoji to the rendered text

--- CreateStringVar/index.js
+++ CreateStringVar/index.js
@@ -1,3 +1,4 @@
+// Create my string
 let username = "nathan"
 let message = "You have three new notifications"
 console.log(message + ", " + username + "!") // concatenate two strings
@@ -5,13 +6,12 @@ console.log(message + ", " + username + "!") // concatenate two strings
 let messageToUser = message +  ", " + username + "!"
 console.log(messageToUser) 
 
-// Create my string
-// let myGreeting = greeting + myName
-// console.log(myGreeting)
-
-
 // Render the message on HTML page
 let welcomeEl = document.getElementById("welcome-el")
 let myName = "Nathan"
 let greeting = "Hi, my name is "
 welcomeEl.innerText = greeting + myName
+
+// Add an emoji to the end
+// welcomeEl.innerText = welcomeEl.innerText + "😛"
+welcomeEl.innerText += "😛"
