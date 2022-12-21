b7b2e5c99b132ec0e2a1671ff99e6ed48eeb0caa
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-20 16:31:20 -0800

    create string

---
+++ CreateStringVar/index.js
@@ -0,0 +1,12 @@
+let username = "nathan"
+let message = "You have three new notifications"
+console.log(message + ", " + username + "!") // concatenate two strings
+
+let messageToUser = message +  ", " + username + "!"
+console.log(messageToUser) 
+
+// Create my string
+let myName = "Nathan"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + myName
+console.log(myGreeting)
