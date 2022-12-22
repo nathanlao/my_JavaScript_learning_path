9becbf5fd6f2d484a3dae4627ae0627d76d62b79
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-22 13:54:00 -0800

    Added button to start game and style button

--- BlackJackGame/index.js
+++ BlackJackGame/index.js
@@ -5,15 +5,17 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 😉"
-} else if(sum === 21) { // Is the sum strictly/exactly equal to 21?
-    message = "Wohoo! You've got Blackjack! 😆"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😰"
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 😉"
+    } else if(sum === 21) { // Is the sum strictly/exactly equal to 21?
+        message = "Wohoo! You've got Blackjack! 😆"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😰"
+        console.log(message)
+        isAlive = false
+    }
     console.log(message)
-    isAlive = false
 }
 
-console.log(message)
