5fc202ae9542934ac3eff25a46b5dc2b815b2bc5
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-21 20:46:25 -0800

    boolean to keep track of state

--- BlackJackGame/index.js
+++ BlackJackGame/index.js
@@ -1,11 +1,16 @@
-let firstCard = 5
-let secondCard = 6
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 😉")
 } else if(sum === 21) { // Is the sum strictly/exactly equal to 21?
     console.log("Wohoo! You've got Blackjack! 😆")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😰")
 }
+
+// CASH OUT!
+console.log(hasBlackJack)
