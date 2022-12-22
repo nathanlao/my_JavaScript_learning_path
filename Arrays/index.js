465b0729ff8e9431eb2310137faccc4be20067b8
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-22 15:12:01 -0800

    added array practice

---
+++ Arrays/index.js
@@ -0,0 +1,30 @@
+// Arrays
+let featurePosts = [
+    "one", 
+    "two", 
+    "three"
+]
+
+let newFeature = "four"
+featurePosts.push(newFeature)
+
+console.log( featurePosts.length )
+
+let myLinkin = [
+    "third year student",
+    "SFU",
+    "JavaScript"
+]
+
+console.log(myLinkin[1])
+console.log(myLinkin[0])
+
+// Arrays with composite/complex data type
+let myself = ["Nathan", 25, true]
+
+// Adding and remiving items from arrays
+let cards = [7, 4]
+cards.push(6)
+
+// remove the last item in an array
+cards.pop()
