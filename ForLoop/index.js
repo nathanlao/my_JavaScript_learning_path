c763b732bfb24c9282e630729f040737d17d816a
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-22 15:32:40 -0800

    For loop and DOM practice

--- ForLoop/index.js
+++ ForLoop/index.js
@@ -16,4 +16,11 @@ for (let i = 0; i < cards.length; i++) {
     console.log(cards[i])
 }
 
+// For loops, arrays and DOM
+let sentence = ["Hello", "my", "name", "is", "Per"]
+let greetingEl = document.getElementById("greeting-el")
+
+for (let i = 0; i < sentence.length; i++) {
+    greetingEl.textContent += sentence[i] + " "
+}
 
