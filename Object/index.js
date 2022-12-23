2f31ccd44b79fbf5ebbf0d022fd5d0d7606e476e
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-22 20:55:57 -0800

    Practices

--- Object/index.js
+++ Object/index.js
@@ -27,3 +27,36 @@ let airbnb = {
 
 console.log(airbnb.title)
 console.log(airbnb.image)
+
+// Practice
+let person = {
+    name: "Nathan",
+    age: 25,
+    country: "HongKong",
+}
+
+function logData() {
+    console.log(person.name + " " + person.country)
+}
+
+logData()
+
+// Arrays
+let countries = ["China", "India", "USA"]
+for (let i = 0; i < countries.length; i++) {
+    console.log("- " + countries[i])
+}
+
+//push & pop -> add/remove item at the end of array
+//unshift & shift -> add/remove item at the start of array
+let testArray = ["Monaco", "India", "USA"]
+testArray.pop()
+testArray.shift()
+testArray.unshift("China")
+
+// function to return a random item in array
+let hands = ["rock", " paper", "scissor"]
+function getHand() {
+    let randomIndex = Math.floor(Math.random() * 3)
+    return hands[randomIndex]
+}
