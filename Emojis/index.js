465eb8e198882f56073fe045473b568285e512e9
nathan <75557717+nathanlao@users.noreply.github.com>
2023-01-02 02:52:07 -0800

    render the new emoji to screen

--- Emojis/index.js
+++ Emojis/index.js
@@ -3,17 +3,23 @@ const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emoji-container")
 const pushBtn = document.getElementById("push-btn")
 
-for (myemoji of myEmojis) {
-    const emoji = document.createElement("span")
-    emoji.textContent = myemoji
-    emojiContainer.append(emoji)
+function renderEmoji() {
+    for (myemoji of myEmojis) {
+        const emoji = document.createElement("span")
+        emoji.textContent = myemoji
+        emojiContainer.append(emoji)
+    }
 }
+
+renderEmoji()
 
 pushBtn.addEventListener("click", function() {
     const emojiInput = document.getElementById("emoji-input")
     if (emojiInput.value) {
         myEmojis.push(emojiInput.value)
         emojiInput.value = " "
+        emojiContainer.innerHTML = ""
+        renderEmoji()
     }
 })
 
