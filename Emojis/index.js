83eee8f9fb1ac5f56a2e6418bc1d0b2af48a89b2
nathan <75557717+nathanlao@users.noreply.github.com>
2023-01-02 02:57:18 -0800

    Added unshift button

--- Emojis/index.js
+++ Emojis/index.js
@@ -1,9 +1,12 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 
 const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
 const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
 
 function renderEmoji() {
+    emojiContainer.innerHTML = ""
     for (myemoji of myEmojis) {
         const emoji = document.createElement("span")
         emoji.textContent = myemoji
@@ -14,11 +17,17 @@ function renderEmoji() {
 renderEmoji()
 
 pushBtn.addEventListener("click", function() {
-    const emojiInput = document.getElementById("emoji-input")
     if (emojiInput.value) {
         myEmojis.push(emojiInput.value)
+        emojiInput.value = " "   
+        renderEmoji()
+    }
+})
+
+unshiftBtn.addEventListener("click", function() {
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
         emojiInput.value = " "
-        emojiContainer.innerHTML = ""
         renderEmoji()
     }
 })
