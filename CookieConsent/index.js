954ee4b6f8581369eb90fbbc91f4eddaa08aa596
nathanlao <gla93@sfu.ca>
2023-01-07 03:40:16 -0800

    Added template string of loading svg and new p when user submit the form using innerHTML

--- CookieConsent/index.js
+++ CookieConsent/index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 // log out the msg after 1.5 sec delay
 setTimeout(function() {
@@ -16,5 +17,15 @@ modalCloseBtn.addEventListener('click', function() {
 // Prevent the default behaviou that trigger the refresh
 consentForm.addEventListener('submit', function(event) {
     event.preventDefault()
-    console.log("form submited")
+    // console.log("form submited")
+    let string = 
+    `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p class="uploadText" id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
+    `
+    modalText.innerHTML = string
 })
